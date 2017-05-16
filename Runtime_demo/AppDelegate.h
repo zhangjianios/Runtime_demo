741b092dbdacc12d0c503c2f85fb58eed73ece9c
Project02 <343438569@qq.com>
2017-05-16 11:18:38 +0800

    Initial Commit

---
+++ Runtime_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime_demo
+//
+//  Created by mac on 17/5/16.
+//  Copyright © 2017年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime_demo
+//
+//  Created by mac on 17/5/16.
+//  Copyright © 2017年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
